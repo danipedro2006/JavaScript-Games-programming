72ac10717ab38983e586bdeb3160385141b4d28d
daniel pirvu <danipedro2006@yahoo.com>
2021-03-14 20:50:34 +0200

    add new feature

--- js/stateMain.js
+++ js/stateMain.js
@@ -6,9 +6,17 @@ var StateMain = {
     game.load.image("green","images/main/blocks/green.png");
     game.load.image("yellow","images/main/blocks/yellow.png");
     game.load.spritesheet("rings","images/main/rings.png",60,65,5);
+    game.load.spritesheet("balls","images/main/balls.png",35,35,5);
     game.scale.forceOrientation(false, true);
   },
   create:function(){
+    this.speed=50;
+    this.incSpeed=20;
+    this.maxSpeed=450;
+    score=0;
+    game.physics.startSystem(Phaser.Physics.Arcade);
+    this.pointSound=game.add.audio("points");
+    this.gameOverSound=game.add.audio("gameOver");
 		var red=game.add.image(0,0,"red");
     var blue=game.add.image(0,100,"blue");
     var green=game.add.image(100,0,"green");
@@ -33,11 +41,40 @@ var StateMain = {
     this.blockGroup.add(green);
     this.blockGroup.add(yellow);
     this.blockGroup.x=game.world.centerX-this.blockGroup.width/2;
-    this.blockGroup.y=game.height-300;
-    this.ring=game.add.image(game.world.centerX, this.blockGroup.y-100,"rings")
+    this.blockGroup.y=game.height-250;
+    //Ring
+    this.ring=game.add.image(game.world.centerX, this.blockGroup.y-150,"rings")
     this.ring.anchor.set(0.5,0.5);
-    this.setListeners();
+  //Ball
+    this.ball=game.add.sprite(0,0,"balls");
+    this.ball.anchor.set(0.5,0.5);
+    game.physics.arcade.enable(this.ball);
 
+    this.scoreText=game.add.text(game.world.centerX, 170, "0");
+    this.scoreText.addColor("#ffffff",0);
+    this.scoreText.fontSize=64;
+    this.scoreText.anchor.set(0.5,0.5);
+
+    this.scoreLabel=game.add.text(game.world.centerX, 130, "score");
+    this.scoreLabel.addColor("#ffffff",0);
+    this.scoreLabel.fontSize=32;
+    this.scoreLabel.anchor.set(0.5,0.5);
+
+    this.setListeners();
+    this.resetBall();
+
+  },
+
+  resetBall:function(){
+    var color=game.rnd.integerInRange(0, 5);
+    var xx=game.rnd.integerInRange(0,game.world.width);
+    var yy=game.rnd.integerInRange(0,100);
+    this.ball.frame=color;
+    this.ball.x=xx;
+    this.ball.y=yy;
+    //this.ball.body.velocity.setTo(0,100);
+    var rot=game.physics.arcade.moveToXY(this.ball,this.ring.x, this.ring.y, this.speed);
+    this.ball.rotation=rot;
   },
 
   setListeners:function(){
@@ -68,6 +105,19 @@ var StateMain = {
 
 
   update:function(){
+    var diffx=Math.abs(this.ring.x-this.ball.x);
+    var diffy=Math.abs(this.ring.y-this.ball.y);
+    if(diffx<10 && diffy<10){
+      this.ball.body.velocity.setTo(0,0);
 
+    if(this.ball.frame == this.ring.frame){
+      this.resetBall();
+      score++;
+      this.scoreText.text=score;
+    }
+    else{
+      game.state.start("StateOver");
+    }
+    }
   }
 }
